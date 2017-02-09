9d1311b3a26ecd2d7cf2f109578e13171030d265
gvoronov <gvoronov@gmail.com>
2017-02-09 14:49:26 -0700

    woot 11 done

--- leetcode/algorithms/problem_011_container_with_most_water.py
+++ leetcode/algorithms/problem_011_container_with_most_water.py
@@ -1,3 +1,6 @@
+import sys
+import itertools
+
 class Solution(object):
     def maxArea(self, height):
         """
@@ -5,13 +8,22 @@ class Solution(object):
         :rtype: int
         """
 
-        max_area = 0
-        for i in range(len(height)):
-            for j in range(len(height) - 1, i, -1):
-                if height[j] >= height[i]:
-                    max_area = max(max_area, height[i] * (j - i))
-                    break
-                max_area = max(max_area, height[j] * (j - i))
+        grouped_height = itertools.groupby(sorted(
+            [(i, h) for i, h in enumerate(height)], key=lambda item: item[1], reverse=True),
+            lambda item: item[1])
+
+        left_window, right_window, max_area = sys.maxint, -1, 0
+        for group in grouped_height:
+            positions = [item[0] for item in group[1]]
+            left_group_window, right_group_window = min(positions), max(positions)
+
+            if left_group_window < left_window:
+                left_window = left_group_window
+
+            if right_group_window > right_window:
+                right_window = right_group_window
+
+            max_area = max(max_area, (right_window - left_window) * group[0])
 
         return max_area
 
