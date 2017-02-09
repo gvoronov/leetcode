18a0813a4970f3623882334499356a1091e75097
gvoronov <gvoronov@gmail.com>
2017-02-08 22:09:49 -0700

    still too slow

--- leetcode/algorithms/problem_011_container_with_most_water.py
+++ leetcode/algorithms/problem_011_container_with_most_water.py
@@ -7,8 +7,11 @@ class Solution(object):
 
         max_area = 0
         for i in range(len(height)):
-            for j in range(i + 1, len(height)):
-                max_area = max(max_area, min(height[i], height[j]) * (j - i))
+            for j in range(len(height) - 1, i, -1):
+                if height[j] >= height[i]:
+                    max_area = max(max_area, height[i] * (j - i))
+                    break
+                max_area = max(max_area, height[j] * (j - i))
 
         return max_area
 
