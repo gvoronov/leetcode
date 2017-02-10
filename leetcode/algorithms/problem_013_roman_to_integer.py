db27798f5a0e57e379e23d57c2277dfe3aff3f66
gvoronov <gvoronov@gmail.com>
2017-02-10 10:40:46 -0700

    13 done

--- leetcode/algorithms/problem_013_roman_to_integer.py
+++ leetcode/algorithms/problem_013_roman_to_integer.py
@@ -5,5 +5,18 @@ class Solution(object):
         :rtype: int
         """
 
+        num = 0
+        for rstr, rval in [('IV', 4), ('IX', 9), ('XL', 40), ('XC', 90), ('CD', 400), ('CM', 900)]:
+            if rstr in s:
+                num += rval
+                s = s.replace(rstr, '')
+
+        for rstr, rval in [('I', 1), ('V', 5), ('X', 10), ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]:
+            tmp_len = len(s)
+            s = s.replace(rstr, '')
+            num += rval * (tmp_len - len(s))
+
+        return num
+
 if __name__ == "__main__":
-    pass
+    print Solution().romanToInt('MMMCMXCIX')
--- leetcode/tests/algorithms/test_013_roman_to_integer.py
+++ leetcode/tests/algorithms/test_013_roman_to_integer.py
@@ -1,20 +1,20 @@
 from leetcode.algorithms.problem_013_roman_to_integer import Solution
 
 def test_01():
-    assert Solution().intToRoman(3999) == "MMMCMXCIX"
+    assert Solution().romanToInt("MMMCMXCIX") == 3999
 
 def test_02():
-    assert Solution().intToRoman(1) == "I"
-    assert Solution().intToRoman(2) == "II"
-    assert Solution().intToRoman(3) == "III"
-    assert Solution().intToRoman(4) == "IV"
-    assert Solution().intToRoman(5) == "V"
-    assert Solution().intToRoman(6) == "VI"
-    assert Solution().intToRoman(7) == "VII"
-    assert Solution().intToRoman(8) == "VIII"
-    assert Solution().intToRoman(9) == "IX"
-    assert Solution().intToRoman(10) == "X"
+    assert Solution().romanToInt("I") == 1
+    assert Solution().romanToInt("II") == 2
+    assert Solution().romanToInt("III") == 3
+    assert Solution().romanToInt("IV") == 4
+    assert Solution().romanToInt("V") == 5
+    assert Solution().romanToInt("VI") == 6
+    assert Solution().romanToInt("VII") == 7
+    assert Solution().romanToInt("VIII") == 8
+    assert Solution().romanToInt("IX") == 9
+    assert Solution().romanToInt("X") == 10
 
 def test_03():
-    assert Solution().intToRoman(400) == "CD"
-    assert Solution().intToRoman(431) == "CDXXXI"
+    assert Solution().romanToInt("CD") == 400
+    assert Solution().romanToInt("CDXXXI") == 431
