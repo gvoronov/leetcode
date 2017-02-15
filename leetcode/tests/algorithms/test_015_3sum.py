9ce31cddfa8c09f57cba2c3bb14c1449325035ca
gvoronov <gvoronov@gmail.com>
2017-02-14 22:50:12 -0700

    added testing for 15, too slow though

--- leetcode/tests/algorithms/test_015_3sum.py
+++ leetcode/tests/algorithms/test_015_3sum.py
@@ -1,6 +1,15 @@
 from leetcode.algorithms.problem_015_3sum import Solution
 
 def test_01():
-    assert Solution().threeSum([-1,0,1,2,-1,-4]) == [
-        [-1, 0, 1], [-1, -1, 2]
-    ]
+    assert set([tuple(l) for l in Solution().threeSum([-1,0,1,2,-1,-4])]) == set([
+        (-1, 0, 1), (-1, -1, 2)
+    ])
+
+def test_02():
+    assert set([tuple(l) for l in Solution().threeSum([-1,0])]) == set([])
+
+def test_03():
+    assert set([tuple(l) for l in Solution().threeSum([])]) == set([])
+
+def test_04():
+    assert set([tuple(l) for l in Solution().threeSum([1,1,1])]) == set([])
