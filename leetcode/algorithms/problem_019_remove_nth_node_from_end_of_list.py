831eddb5b715de8784301819a7f26644ec27a65e
gvoronov <gvoronov@gmail.com>
2017-03-28 21:19:33 -0600

    19 done

--- leetcode/algorithms/problem_019_remove_nth_node_from_end_of_list.py
+++ leetcode/algorithms/problem_019_remove_nth_node_from_end_of_list.py
@@ -1,3 +1,28 @@
+def list_to_listnode(l):
+    if len(l) == 0:
+        return None
+
+    l_it = iter(l)
+
+    head_node = ListNode(l_it.next())
+    node = head_node
+
+    for val in l_it:
+        node.next = ListNode(val)
+        node = node.next
+
+    return head_node
+
+
+def listnode_to_list(ln):
+    l = list()
+    node = ln
+    while node is not None:
+        l.append(node.val)
+        node = node.next
+
+    return l
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
@@ -12,5 +37,27 @@ class Solution(object):
         :rtype: ListNode
         """
 
+        tail_start_node = head
+        tail_end_node = head
+        for i in range(n):
+            tail_end_node = tail_end_node.next
+
+        if tail_end_node is None:
+            return head.next
+
+        while tail_end_node.next is not None:
+            tail_start_node = tail_start_node.next
+            tail_end_node = tail_end_node.next
+
+        tail_start_node.next = tail_start_node.next.next
+
+        return head
+
+
 if __name__ == "__main__":
-    pass
+    print listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 1))
+    print listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 2))
+    print listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 3))
+    print listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 4))
+    print listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 5))
+    print listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1]), 1))
--- leetcode/tests/algorithms/test_019_remove_nth_node_from_end_of_list.py
+++ leetcode/tests/algorithms/test_019_remove_nth_node_from_end_of_list.py
@@ -1,28 +1,29 @@
 from leetcode.algorithms.problem_019_remove_nth_node_from_end_of_list import Solution
 from leetcode.algorithms.problem_019_remove_nth_node_from_end_of_list import ListNode
-
-def list_to_listnode(l):
-    l_it = iter(l)
-
-    linked_list = ListNode(l_it.next())
-    for val in l:
-        linked_list.next = ListNode(val)
-
-    return linked_list
-
-
-def listnode_to_list(ln):
-    l = list()
-    node = ln
-    while node is not None:
-        l.append(node.val)
-        node = node.next
-
-    return l
+from leetcode.algorithms.problem_019_remove_nth_node_from_end_of_list import list_to_listnode
+from leetcode.algorithms.problem_019_remove_nth_node_from_end_of_list import listnode_to_list
 
 def test_01():
-    pass
+    assert listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 1)) == [1, 2, 3, 4]
+
+def test_02():
+    assert listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 2)) == [1, 2, 3, 5]
+
+def test_03():
+    assert listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 3)) == [1, 2, 4, 5]
+
+def test_04():
+    assert listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 4)) == [1, 3, 4, 5]
+
+def test_05():
+    assert listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1, 2, 3, 4, 5]), 5)) == [2, 3, 4, 5]
+
+def test_06():
+    assert listnode_to_list(Solution().removeNthFromEnd(list_to_listnode([1]), 1)) == []
 
 if __name__ == "__main__":
     # print list_to_listnode([1, 2, 3, 4, 5]).val
     print listnode_to_list(list_to_listnode([1, 2, 3, 4, 5]))
+    print listnode_to_list(list_to_listnode([1]))
+    print listnode_to_list(list_to_listnode([1, 3, 5]))
+    print listnode_to_list(list_to_listnode([]))
